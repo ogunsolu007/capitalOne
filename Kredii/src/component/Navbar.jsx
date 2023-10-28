2135506f3926d5764f3f8affbda9be59021f7dcf
ogunsolu qudus <ogunsolu30@gmail.com>
2023-10-28 16:55:50 +0100

    Kredii

---
+++ Kredii/src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h3 className="logo">Kredii</h3>
+      <button>Login</button>
+    </div>
+  );
+};
+export default Navbar;
---
+++ Kredii/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import Navbar from "../component/Navbar";
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-section">
+        <div className="home-content">
+              <img src="/kredii.png" alt="kredii" className="home-img" />
+        <h2>Your credit score is your financial passport.</h2>
+        <p>
+          Think of your credit score as your passport to financial success. It
+          opens doors to credit, loans, and opportunities for growth. It's more
+          than just a number; it's a symbol of your financial trustworthiness.
+          To unlock these opportunities and secure a brighter financial future,
+          all you need to do is check your credit score. Take action today and
+          discover how your financial passport can help you achieve your goals.
+        </p>
+        </div>
+      
+      </div>
+    </>
+  );
+};
+export default Home;
